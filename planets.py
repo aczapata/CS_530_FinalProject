a681c5faca5be8470a2e12520613ac42544157e9
Andrea Zapata <andreaczapata@gmail.com>
2021-04-26 14:53:56 -0400

    Fix calendar date

--- planets.py
+++ planets.py
@@ -254,9 +254,9 @@ class Ui_MainWindow(object):
 		#Date Picker
 		self.date_textbox = QDateTimeEdit()
 		self.date_textbox.setCalendarPopup(True)
-		self.date_textbox.setMinimumDate(QDate(2020, 12, 17))
+		self.date_textbox.setMinimumDate(QDate(1858, 10, 17))
 		self.date_textbox.setMaximumDate(QDate(2050, 12, 31))
-		self.date_textbox.setDate(QDate.currentDate())
+		self.date_textbox.setDate(QDate(2020, 12, 17))
 		
 		#Combo box
 		self.obj_focus = QComboBox()
